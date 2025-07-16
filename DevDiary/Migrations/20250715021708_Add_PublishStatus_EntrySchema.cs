9e56a98825c126657e642ae4545941a49bff3103
Pramesh Pradhan <pra.mesh.dhan@hotmail.com>
2025-07-16 23:04:55 +0545

    migration add publish status

---
+++ DevDiary/Migrations/20250715021708_Add_PublishStatus_EntrySchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevDiary.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_PublishStatus_EntrySchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "DiaryEntries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "DiaryEntries");
+        }
+    }
+}
